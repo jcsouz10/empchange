a3e9a82f3ba2136195368d0c18ead07ec5065914
Jean Souza <jcsouz@br.ibm.com>
2018-05-02 17:07:53 -0300

    Create button edit

--- src/Body.js
+++ src/Body.js
@@ -3,25 +3,60 @@ import './Body.css';
 import { Link } from "react-router-dom";
 
 class Body extends React.Component {
-    getName = () => {
-      const { employee, add } = this.props;
-      return employee.map(name => (
-        <div className='item'>
-            <Link className="link" to={`/user/${name.id}`}>
-            <div  onClick={() => add(name)} key={name.id}>
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      employeeBody: this.props.employee,
+    }
+  }
+
+
+  getName = () => {
+    const { employee, add } = this.props;
+    const {employeeBody} = this.state;
+    return employee.map(name => (
+      <div className='item'>
+        <Link className="link" to={`/user/${name.id}`}>
+          <div onClick={() => add(name)} key={name.id}>
             <img className="img"
               src={`https://picsum.photos/${name.id}`}
             />
-            </div>
-            <h1 className="name2"> {name.name} </h1>
-            </Link>
-        </div>
-      ));
-    };
-  
-    render() {
-      return <div className="body">{this.getName()}</div>;
-    }
+          </div>
+          <h1 className="name2"> {name.name} </h1>
+        </Link>
+      </div>
+    ));
+  };
+
+
+  getValueInput = (evt) => {
+    const inputValue = evt.target.value;
+    this.setState({ input: inputValue });
+    this.filterNames(inputValue);
+    console.log(this.state.employeeBody)
   }
-  
+
+  filterNames (inputValue) {
+  const { employee } = this.props;
+  this.setState({
+    employeeBody: employee.filter(item => 
+       item.name.includes(inputValue))
+  });
+  }
+
+  render() {
+    return (
+    <div>
+      <div className="body">
+        {this.getName()}
+      </div> 
+      <div className='input'>
+        <input type="text" onChange={this.getValueInput} /> 
+      </div>
+    </div>
+    )
+  }
+}
+
 export default Body;
--- src/User.js
+++ src/User.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import './User.css';
+import axios from 'axios';
 
 class User extends React.Component {
-    
-    render() {
-      return <div className='render'>
-      {this.props.employeeCurrent.map(name=><div>
-      <div className='profile'>
-        <img className="img2" src={`https://picsum.photos/${name.id}`}/>
-        <div className='name'> 
-          <h3>{name.name}</h3>
-          <p> {name.position} </p>
-        </div>
-      </div>
-        <div className='container'>
-          <div className='item2'>  Manager: {name.manager} </div>
-          <div className='item2'>   Site: {name.site}  </div>
-          <div className='item2'>   Start Day: {name.start_day} </div>
-        </div>
-      </div>)}
-      </div>;
+  constructor() {
+    super();
+
+    this.state = {
+      newValue: "",
     }
   }
-export default User;
+
+  onSubmitDate = async event => {
+    const newUrl = prompt("Please with new URL:");
+    const personCurrent = event.target.value;
+
+    axios.patch(`http://localhost:3004/employee/${personCurrent}`, {
+      url_git: newUrl
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+
+  render() {
+    return <div className='render'>
+      {this.props.employeeCurrent.map(name => <div>
+        <div className='profile'>
+          <img className="img2" src={`https://picsum.photos/${name.id}`} />
+          <div className='name'>
+            <h3>{name.name}</h3>
+            <p> {name.position} </p>
+          </div>
+        </div>
+        <div className='container'>
+          <div className='item2'>   Manager: {name.manager} </div>
+          <div className='item2'>   Site: {name.site} </div>
+          <div className='item2'>   Start Day: {name.start_day}  </div>
+          <div className='item2'>   GitHub:<a className='item2Link' href={`${name.url_git}`}>{name.url_git} </a>  <button className='itembtt' onClick={this.onSubmitDate} id={name.id} value={name.id}> Edit </button> </div>
+          <div className='item2'>   CellPhone: {name.cellphone} <button onClick={this.teste}> Edit </button> </div>
+          <div className='item2'>   End Day: {name.end_day}</div>
+        </div>
+      </div>)}
+    </div>;
+  }
+}
+export default User; 
